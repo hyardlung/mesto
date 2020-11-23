5ca07fd9ff16f1b6ee6a3e4503fd58d3bcd673c6
Dmitry Volkov <hyardlung@gmail.com>
2020-11-23 23:07:36 +0700

    fix(popup): дать в querySelector сложный селектор
    Модификатор в селекторе по классу получился и не булевый,
    и не "ключ - значение". Решено было передать в querySelector
    сложный селектор из класса и атрибута.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let profileEditButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
 
-let nameInput = document.querySelector('.popup__input_name');
-let aboutInput = document.querySelector('.popup__input_about');
+let nameInput = document.querySelector('.popup__input[name="profileName"]');
+let aboutInput = document.querySelector('.popup__input[name="profileAbout"]');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 
