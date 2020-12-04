228c849d652b4dac4d7af297ac8b095c4ac69804
Dmitry Volkov <hyardlung@gmail.com>
2020-12-04 21:06:19 +0700

    feat(add-card): добавить попап добавления карточки
    - Добавил в вёрстку форму добавления новых карточек
    - Реализовал открытие попапа по нажатию на кнопку с плюсом и закрытие по
      крестику, аналогично попапу редактирования профиля

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ let profileEditButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
 
+const popupAddCard = document.querySelector('.popup_add-card');
+const addCardButton = document.querySelector('.profile__add-button');
+const popupAddCardCloseButton = document.querySelector('.popup__close-button_add-card');
+
+
 let nameInput = document.querySelector('.popup__input[name="profileName"]');
 let aboutInput = document.querySelector('.popup__input[name="profileAbout"]');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
+
+const cardNameInput = document.querySelector('.popup__input[name="cardName"]');
+const cardLinkInput = document.querySelector('.popup__input[name="cardLink"]');
 
 let popupForm = document.querySelector('.popup__form');
 
@@ -55,27 +63,36 @@ function composeCard(item) {
   return newCard;
 }
 
-
-function openPopup() {
-  popup.classList.add('popup_opened');
+function openEditProfilePopup() {
   nameInput.value = profileName.textContent;
   aboutInput.value = profileAbout.textContent;
+  popup.classList.add('popup_opened');
 }
 
-function closePopup() {
+function closeEditProfilePopup() {
   popup.classList.remove('popup_opened');
+}
+
+function openAddNewCardPopup() {
+  popupAddCard.classList.add('popup_opened');
+}
+
+function closeAddNewCardPopup() {
+  popupAddCard.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = aboutInput.value;
-  closePopup()
+  closeEditProfilePopup()
 }
 
-profileEditButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
-
+profileEditButton.addEventListener('click', openEditProfilePopup);
+popupCloseButton.addEventListener('click', closeEditProfilePopup);
 popupForm.addEventListener('submit', formSubmitHandler);
+
+addCardButton.addEventListener('click', openAddNewCardPopup);
+popupAddCardCloseButton.addEventListener('click', closeAddNewCardPopup)
 
 renderList();
