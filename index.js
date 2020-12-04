bf032fdff01633717f9e6514a37c2f1c1fc1590c
Dmitry Volkov <hyardlung@gmail.com>
2020-12-04 13:59:07 +0700

    feat: добавить дефолтные карточки через js
    Убрал из вёрстки все добавленные вручную карточки, оставил только тег
    <template> с шаблоном вёрстки, который подтягивается из js, клонируется
    и наполняется контентом из массива.

--- index.js
+++ index.js
@@ -10,6 +10,51 @@ let profileAbout = document.querySelector('.profile__about');
 
 let popupForm = document.querySelector('.popup__form');
 
+const cardsContainerElement = document.querySelector('.elements__list');
+const templateCard = document.querySelector('.elements__template');
+
+const initialCards = [
+  {
+    name: 'Сундуки',
+    link: './images/sundki.jpeg'
+  },
+  {
+    name: 'Хаыйракан',
+    link: './images/hayirakan.jpeg'
+  },
+  {
+    name: 'Саяно-Шушенская ГЭС',
+    link: './images/sayano-shushenskaya-ges.jpg'
+  },
+  {
+    name: 'Ленские столбы',
+    link: './images/lenskie-stolby.jpeg'
+  },
+  {
+    name: 'Ергаки',
+    link: './images/ergaki.jpg'
+  },
+  {
+    name: 'Курганская пещера',
+    link: './images/kungurskaya-peshera.jpeg'
+  }
+];
+
+// первоначальный рендер списка карточек
+function renderList() {
+  const cardsList = initialCards.map(composeCard);
+  cardsContainerElement.append(...cardsList);
+}
+
+function composeCard(item) {
+  const newCard = templateCard.content.cloneNode(true);
+  const cardHeading = newCard.querySelector('.card__heading');
+  const cardImage = newCard.querySelector('.card__image');
+  cardHeading.textContent = item.name;
+  cardImage.src = item.link;
+  return newCard;
+}
+
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -32,3 +77,5 @@ profileEditButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
 
 popupForm.addEventListener('submit', formSubmitHandler);
+
+renderList();
