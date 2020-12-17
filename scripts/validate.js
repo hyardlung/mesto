f60bfd824cb0bb47866c298cf189c10497c1b8a3
Dmitry Volkov <hyardlung@gmail.com>
2020-12-18 06:34:22 +0700

    feat(validate): дописать валидацию второго инпута
    Оба инпута попапа редактирования профиля валидируются.

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,35 +1,57 @@
 const formElement = document.querySelector('.popup__form');
 const formInput = formElement.querySelector('.popup__input');
-const formError = formElement.querySelector(`.${formInput.id}-error`);
 
 // показать красный нижний бордер при ошибке валидации инпута
-const showInputError = (element, errorMessage) => {
-  element.classList.add('popup__input_type_error');
-  formError.textContent = errorMessage;
-  formError.classList.add('popup__input-error_active');
-
-}
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
 
 // скрыть тот же бордер при пройденной валидации
-const hideInputError = (element) => {
-  element.classList.remove('popup__input_type_error');
-  formError.classList.remove('popup__input-error_active');
-  formError.textContent = '';
-}
+const hideInputError = (inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
 
 // проверка валидности поля
-const isValid = () => {
-  if (!formInput.validity.valid) {
-    showInputError(formInput, formInput.validationMessage);
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
-    hideInputError(formInput);
+    hideInputError(formElement, inputElement);
   }
+};
+
+// добавление обработчиков всем полям формы
+const setEventListener = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListener(formElement);
+  });
 }
+enableValidation();
+
 
 //отмена стандартного поведения формы
 formElement.addEventListener('submit', (evt) => {
   evt.preventDefault();
-})
+});
 
 //проверяем инпуты при вводе символов
 formInput.addEventListener('input', isValid);
