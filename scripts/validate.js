b2b7db98d5325c0e5fe16836647cffea2afbfd6b
Dmitry Volkov <hyardlung@gmail.com>
2020-12-20 01:14:48 +0700

    feat(validate): написать конфиг валидации
    - Написал конфиг валидации, который представляет из себя объект,
      содержащий необходимые классы в виде значений ключей, которые
      пробрасываются во все функции, делая более гибкими захардокоденные
      места.
    - Поправил ховер сабмитящей кнопки, теперь она не меняется при
    наведении будучи отключенной.
    - Отключил в вёрстке автозаполнение инпутов.

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,66 +1,77 @@
 const formElement = document.querySelector('.popup__form');
 
 // показать красный нижний бордер и текст при ошибке валидации инпута
-function showInputError(form, input) {
+function showInputError(form, input, config) {
   const errorElement = form.querySelector(`#${input.id}-error`);
   errorElement.textContent = input.validationMessage;
-  input.classList.add('popup__input_state_invalid');
+  input.classList.add(config.inputInvalidClass);
 }
 
 // скрыть тот же бордер и текст ошибки при пройденной валидации
-function hideInputError(form, input) {
+function hideInputError(form, input, config) {
   const errorElement = form.querySelector(`#${input.id}-error`);
   errorElement.textContent = '';
-  input.classList.remove('popup__input_state_invalid');
+  input.classList.remove(config.inputInvalidClass);
 }
 
 // проверка валидности поля
-function isValid(checkForm, checkInput) {
+function isValid(checkForm, checkInput, config) {
   if (!checkInput.validity.valid) {
-    showInputError(checkForm, checkInput)
+    showInputError(checkForm, checkInput, config)
   } else {
-    hideInputError(checkForm, checkInput);
+    hideInputError(checkForm, checkInput, config);
   }
 }
 
 // проверка состояния кнопки submit
-function setButtonState(button, isActive) {
+function setButtonState(button, isActive, config) {
   if (!isActive) {
-    button.classList.add('popup__save-button_invalid');
+    button.classList.add(config.buttonInvalidClass);
     button.disabled = true;
   } else {
-    button.classList.remove('popup__save-button_invalid');
+    button.classList.remove(config.buttonInvalidClass);
     button.disabled = false;
   }
 }
 
 // валидация формы
-function setEventListener(form) {
-  const inputList = form.querySelectorAll('.popup__input');
-  const submitButton = form.querySelector('.popup__save-button');
+function setEventListener(form, config) {
+  const inputList = form.querySelectorAll(config.inputSelector);
+  const submitButton = form.querySelector(config.submitButtonSelector);
   // слушатель с проверкой валидности для каждого импута
   inputList.forEach(currentInput => {
-    currentInput.addEventListener('input', (evt) => {
-      isValid(form, currentInput);
-      setButtonState(submitButton, form.checkValidity());
+    currentInput.addEventListener('input', () => {
+      isValid(form, currentInput, config);
+      setButtonState(submitButton, form.checkValidity(), config);
     })
   })
 }
 
 
 // применение валидации к открытой форме
-function enableValidation() {
-  const forms = document.querySelectorAll('.popup__form');
+function enableValidation(config) {
+  const forms = document.querySelectorAll(config.formSelector);
   forms.forEach(form => {
-    setEventListener(form);
+    setEventListener(form, config);
+
     // сброс дефолтного поведения
     form.addEventListener('submit', (evt) => {
       evt.preventDefault();
     });
+
     // проверка состояния кнопки при открытии формы
-    const submitButton = form.querySelector('.popup__save-button');
-    setButtonState(submitButton, form.checkValidity());
+    const submitButton = form.querySelector(config.submitButtonSelector);
+    setButtonState(submitButton, form.checkValidity(), config);
   });
 }
 
-enableValidation()
+// конфиг валидации
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-button',
+  inputInvalidClass: 'popup__input_state_invalid',
+  buttonInvalidClass: 'popup__save-button_invalid'
+}
+
+enableValidation(validationConfig)
