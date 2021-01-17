7a546ef08926f577ea01d2275debe6a9956cdc87
Dmitry Volkov <hyardlung@gmail.com>
2021-01-17 23:29:44 +0700

    refactor: вернуть предпросмотр в класс Card
    *  Вернулся к предыдущему коммиту, где предпросмотр изображения
    находился внутри Card. Так выглядит лучше.
    *  Удалил из Card переменную previewCloseButton. Функционал,
       описывающий работу с крестиком попапа уже реализован в index.js.
    *  По этой же причине удалён слушатель нажатия на эту кнопку.
    *  Добавил в _handleOpenPreview строку для альтов картинок. Без них
       не то пальто.
    *  Также добавил строку для альтов в публичный generateCard.
    *  Причесал комментарии к коду.

--- scripts/components/Card.js
+++ scripts/components/Card.js
@@ -1,8 +1,6 @@
 const popupPreview = document.querySelector('.popup_preview');
-
 const previewImage = popupPreview.querySelector('.preview__image');
 const previewCaption = popupPreview.querySelector('.preview__caption');
-const previewCloseButton = popupPreview.querySelector('.popup__close-button_preview');
 
 export default class Card {
   constructor(data, cardSelector) {
@@ -22,32 +20,15 @@ export default class Card {
     return cardTemplate;
   }
 
-  // подготовим карточку к публикации
-  generateCard() {
-    // Запишем разметку в приватное поле _element.
-    // Так у других элементов появится доступ к ней.
-    this._element = this._getTemplate();
-    this._setEventListeners();
-    // Добавим данные
-    this._element.querySelector('.card__heading').textContent = this._name;
-    this._element.querySelector('.card__image').src = this._image;
-    // Вернём элемент наружу
-    return this._element;
-  }
-
   // открытие предпросмотра изображения
   _handleOpenPreview() {
-    previewCaption.textContent = this._name;
     previewImage.src = this._image;
+    previewImage.alt = this._name;
+    previewCaption.textContent = this._name;
     popupPreview.classList.add('popup_opened');
   }
 
-  // закрытие предпросмотра по клику на крестик
-  _handleClosePreview() {
-    popupPreview.classList.remove('popup_opened');
-  }
-
-  // лайк карточки
+  // лайк карточки (вкл/выкл)
   _cardLikeToggle(evt) {
     evt.target.classList.toggle('card__like-button_active');
   }
@@ -62,14 +43,26 @@ export default class Card {
     this._element.querySelector('.card__image').addEventListener('click', () => {
       this._handleOpenPreview();
     })
-    previewCloseButton.addEventListener('click', () => {
-      this._handleClosePreview();
-    })
     this._element.querySelector('.card__like-button').addEventListener('click', (evt) => {
       this._cardLikeToggle(evt);
     })
     this._element.querySelector('.card__remove-button').addEventListener('click', (evt) => {
       this._removeCard(evt);
     })
+  }
+
+  // подготовим карточку к публикации
+  generateCard() {
+    // Запишем разметку в приватное поле _element.
+    // Так у других элементов появится доступ к ней.
+    this._element = this._getTemplate();
+    // Добавим данные
+    this._element.querySelector('.card__image').src = this._image;
+    this._element.querySelector('.card__image').alt = this._name;
+    this._element.querySelector('.card__heading').textContent = this._name;
+
+    this._setEventListeners();
+    // Вернём элемент наружу
+    return this._element;
   }
 }
