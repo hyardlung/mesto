8165a3806ca287637bf2d309d67e8de8e988e592
Dmitry Volkov <hyardlung@gmail.com>
2021-01-17 16:00:34 +0700

    refactor: вынести превью карточек из класса Card
    Попап с предпросмотром карточки не должен относиться к классу Card, хоть
    и велик соблазн запихнуть его именно туда.  Вынес в отдельную функцию
    код, который отвечает за открытие попапа и подтягивание туда картинки и
    подписи. С последним пока проблема: не сообразил, как реализовать вывод
    в описание картинки названия карточки. Оставил комментарий в коде с
    TODO на будущее.

--- scripts/components/Card.js
+++ scripts/components/Card.js
@@ -1,9 +1,3 @@
-const popupPreview = document.querySelector('.popup_preview');
-
-const previewImage = popupPreview.querySelector('.preview__image');
-const previewCaption = popupPreview.querySelector('.preview__caption');
-const previewCloseButton = popupPreview.querySelector('.popup__close-button_preview');
-
 export default class Card {
   constructor(data, cardSelector) {
     this._name = data.name;
@@ -35,18 +29,6 @@ export default class Card {
     return this._element;
   }
 
-  // открытие предпросмотра изображения
-  _handleOpenPreview() {
-    previewCaption.textContent = this._name;
-    previewImage.src = this._image;
-    popupPreview.classList.add('popup_opened');
-  }
-
-  // закрытие предпросмотра по клику на крестик
-  _handleClosePreview() {
-    popupPreview.classList.remove('popup_opened');
-  }
-
   // лайк карточки
   _cardLikeToggle(evt) {
     evt.target.classList.toggle('card__like-button_active');
@@ -59,12 +41,6 @@ export default class Card {
 
   // слушатели кликов
   _setEventListeners() {
-    this._element.querySelector('.card__image').addEventListener('click', () => {
-      this._handleOpenPreview();
-    })
-    previewCloseButton.addEventListener('click', () => {
-      this._handleClosePreview();
-    })
     this._element.querySelector('.card__like-button').addEventListener('click', (evt) => {
       this._cardLikeToggle(evt);
     })
