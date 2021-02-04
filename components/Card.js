2e4030181c56f37ca8c9a216c00e895cefc3fddc
Dmitry Volkov <hyardlung@gmail.com>
2021-02-04 21:15:36 +0700

    refactor: написать класс PopupWithForm
    Фуф, нужно чаще коммитить. Здесь получилось очень много всякого. Прежде
    всего написан и выполняет свои функции класс PopupWithForm, наследуемый
    от Popup. Он помимо прочего принимает в конструктор колбэк сабмита
    формы. Работает пока только с формой добавления карточек, на очереди
    редактирование профиля.
    Помимо прочего:
    
    **Card**
    * Расширил метод generateCard(), дополнил парочкой переменных для
      удобства.
    * То же самое сделал в _setEventListeners(), определил элементы лайка,
      корзины удаления и картинку карточку в переменные, чтобы было
      удобнее к ним обращаться.
    
    **Section**
    * В методе addItem() поменял append на prepend чтобы картинка
      вставлялись в начало сетки, а не в конец.
    
    **Popup**
    * this._popup теперь ссылается на параметр конструктора, а не на
      DOM-элемент с параметром.
    
    **PopupWithImage**
    * Переписал логику класса, отказался от завязки на event.target
      по картинке карточки.
    
    **index.js**
    * Разброд и шатание в импортах. Временно.
    * Переписаны инстансы классов в соответствии с их новым
      содержанием.
    * Есть заготовка под попап редактирования профиля, но не
      работает :)

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,3 @@
-
-
 export default class Card {
   constructor({card, handleOpenPreview}, cardSelector) {
     this._name = card.name;
@@ -10,22 +8,13 @@ export default class Card {
 
   // метод забирающий разметку из HTML и клонирующий элемент
   _getTemplate() {
-    const cardTemplate = document
-      .querySelector(this._cardSelector)
+    const cardTemplate = this._cardSelector
       .content
       .querySelector('.card')
       .cloneNode(true);
     // вернём DOM-элемент карточки
     return cardTemplate;
   }
-
-  // // метод открывающий предпросмотр изображения         TODO: перенести в index.js, сделать функцией, которая передаётся параметром в конструктор чтобы избавиться от кольцевого импорта
-  // _handleOpenPreview() {
-  //   previewImage.src = this._image;
-  //   previewImage.alt = this._name;
-  //   previewCaption.textContent = this._name;
-  //   openPopup(popupPreview);
-  // }
 
   // метод установки/снятия лайка карточки
   _cardLikeToggle(evt) {
@@ -38,37 +27,36 @@ export default class Card {
     this._element = null;
   }
 
-  // слушатели кликов
-  _setEventListeners() {
-
-    // слушатель клика по картинке карточки
-    this._element.querySelector('.card__image').addEventListener('click', (evt) => {
-      this._handleOpenPreview(evt);
-    });
-
-    // слушатель клика по кнопке лайка
-    this._element.querySelector('.card__like-button').addEventListener('click', (evt) => {
-      this._cardLikeToggle(evt);
-    });
-
-    // слушатель клика по корзине (кнопке удаления карточки)
-    this._element.querySelector('.card__remove-button').addEventListener('click', (evt) => {
-      this._removeCard(evt);
-    })
-  }
-
   // метод генерирующий карточку и готовящий её к публикации
   generateCard() {
     // Запишем разметку в приватное поле _element.
     // Так у других элементов появится доступ к ней.
     this._element = this._getTemplate();
     // Добавим данные
-    this._element.querySelector('.card__image').src = this._image;
-    this._element.querySelector('.card__image').alt = this._name;
-    this._element.querySelector('.card__heading').textContent = this._name;
+    this._cardImage = this._element.querySelector('.card__image');
+    this._cardCaption = this._element.querySelector('.card__heading');
 
+    this._cardImage.src = this._image;
+    this._cardImage.alt = this._name;
+    this._cardCaption.textContent = this._name;
     this._setEventListeners();
     // Вернём элемент наружу
     return this._element;
+  }
+
+  // слушатели кликов
+  _setEventListeners() {
+    this._cardImage = this._element.querySelector('.card__image');
+    this._cardLike = this._element.querySelector('.card__like-button');
+    this._remove = this._element.querySelector('.card__remove-button');
+
+    // слушатель клика по картинке карточки
+    this._cardImage.addEventListener('click', () => {this._handleOpenPreview(this._element)});
+
+    // слушатель клика по кнопке лайка
+    this._cardLike.addEventListener('click', (evt) => {this._cardLikeToggle(evt)});
+
+    // слушатель клика по корзине (кнопке удаления карточки)
+    this._remove.addEventListener('click', (evt) => {this._removeCard(evt)})
   }
 }
